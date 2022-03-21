b627dd9d96a28db121e1d51afb5d801dbc640464
Amrit <AmritMaan2001@gmail.com>
2022-03-21 19:41:35 -0400

    initilized repo

---
+++ app/src/main/java/com/example/trippy_trip_planner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.trippy_trip_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
