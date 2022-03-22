541fe4cd020671d75bf443ea8ed0e0210a067fdc
Amrit <AmritMaan2001@gmail.com>
2022-03-21 20:29:35 -0400

    bottom navigation menu layout setup

---
+++ app/src/main/java/com/example/trippy_trip_planner/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.trippy_trip_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        //We will start the main activity using Thread
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
