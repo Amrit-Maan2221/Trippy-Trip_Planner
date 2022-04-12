23f9ef8a060d0cbf4e171e43811c55bc64402ab6
Amrit <AmritMaan2001@gmail.com>
2022-04-12 07:01:33 -0400

    Made layout of recycler View Home

--- app/src/main/java/com/example/trippy_trip_planner/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/trippy_trip_planner/Fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.trippy_trip_planner.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import com.example.trippy_trip_planner.R;
 
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -21,6 +23,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.homeRecyclerView);
+
 
         return view;
     }
