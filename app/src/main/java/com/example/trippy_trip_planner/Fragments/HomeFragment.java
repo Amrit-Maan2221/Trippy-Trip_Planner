72dcb409013200b23e570ea7ab89dc25227a8fb8
Amrit <AmritMaan2001@gmail.com>
2022-04-12 07:35:14 -0400

    adapter set up to show recycler view items and also showing up some hardcoded items

--- app/src/main/java/com/example/trippy_trip_planner/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/trippy_trip_planner/Fragments/HomeFragment.java
@@ -3,13 +3,18 @@ package com.example.trippy_trip_planner.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.trippy_trip_planner.Adapter.TripAdapter;
+import com.example.trippy_trip_planner.Model.Trip;
 import com.example.trippy_trip_planner.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
@@ -25,6 +30,18 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         recyclerView = view.findViewById(R.id.homeRecyclerView);
+
+        ArrayList<Trip> tripList = new ArrayList<>();
+        tripList.add(new Trip("Kashmir", "trip to kashmir", "21/5/2022", "9:22"));
+        tripList.add(new Trip("Kashmir", "trip to kashmir", "21/5/2022", "9:22"));
+        tripList.add(new Trip("Kashmir", "trip to kashmir", "21/5/2022", "9:22"));
+        tripList.add(new Trip("Kashmir", "trip to kashmir", "21/5/2022", "9:22"));
+
+        TripAdapter adapter = new TripAdapter(tripList, getContext());
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
 
 
         return view;
