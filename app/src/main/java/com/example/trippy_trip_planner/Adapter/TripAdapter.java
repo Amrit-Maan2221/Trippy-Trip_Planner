01b8939f1d6e868ed176de1341b8bc8967ab0389
Amrit <AmritMaan2001@gmail.com>
2022-04-12 07:26:31 -0400

    created adapter for trips

---
+++ app/src/main/java/com/example/trippy_trip_planner/Adapter/TripAdapter.java
@@ -0,0 +1,60 @@
+package com.example.trippy_trip_planner.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trippy_trip_planner.Model.Trip;
+import com.example.trippy_trip_planner.R;
+
+import java.util.ArrayList;
+
+public class TripAdapter extends RecyclerView.Adapter<TripAdapter.viewHolder> {
+
+    ArrayList<Trip> tripList;
+    Context context;
+
+    public TripAdapter(ArrayList<Trip> tripList, Context context) {
+        this.tripList = tripList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.trips_list_recycler_layout, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        Trip trip = tripList.get(position);
+        holder.tripName.setText(trip.getTripName());
+        holder.tripDate.setText(trip.getTripDate());
+        holder.tripTime.setText(trip.getTripTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tripList.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        TextView tripName, tripDate, tripTime;
+        Button btnTripLocation;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            tripName = itemView.findViewById(R.id.tvrTripName);
+            tripDate = itemView.findViewById(R.id.tvrTripDate);
+            tripTime = itemView.findViewById(R.id.tvrTripTime);
+            btnTripLocation = itemView.findViewById(R.id.btnStartTrip);
+
+        }
+    }
+}
